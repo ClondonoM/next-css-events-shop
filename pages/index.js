b89bdb93a37ac23dc5a9c24b27451805709a9ba2
Carlos Londono <c.londono.m@gmail.com>
2022-08-21 20:08:31 -0500

    adding and styling posters

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Poster from '../components/Poster';
 
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='container'>
       <Layout page='Home'>
-        <h1>CENA | MUSICA | TRAGOS</h1>
+        <div className={styles.banner}>
+          <h1>COMPRA TUS ANTICIPADAS</h1>
+          <div className={styles.banner_buttons}>
+            <div className={`${styles.banner_aButton}  gradientColor`}>
+              <Link href='viernes'>UNP VIERNES</Link>
+            </div>
+            <div className={`${styles.banner_aButton}  gradientColor`}>
+              <Link href='sabado'>UNP SABADO</Link>
+            </div>
+          </div>
+        </div>
+        <div className={styles.esta_semana_title}>
+          <h1>ESTA SEMANA</h1>
+        </div>
+        <div>
+          <Poster dia={VIERNES} />
+        </div>
       </Layout>
     </div>
   );
